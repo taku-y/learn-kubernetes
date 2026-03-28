82031cf30ca73e1abdef0e66ef41232ac9ac0b57
taku-y <taku.yoshioka.4096@gmail.com>
2026-03-29 00:35:25 +0900

    Fix command

--- learn3/test-concurrent-write.sh
+++ learn3/test-concurrent-write.sh
@@ -6,14 +6,14 @@ kubectl run pod-a --image=amazon/aws-cli --restart=Never \
   --env="AWS_ACCESS_KEY_ID=minioadmin" \
   --env="AWS_SECRET_ACCESS_KEY=minioadmin" \
   --env="AWS_DEFAULT_REGION=us-east-1" \
-  -- aws --endpoint-url http://minio.minio.svc:9000 \
+  -- --endpoint-url http://minio.minio.svc:9000 \
   s3 cp /etc/hostname s3://test-bucket/pod-a.txt &
 
 kubectl run pod-b --image=amazon/aws-cli --restart=Never \
   --env="AWS_ACCESS_KEY_ID=minioadmin" \
   --env="AWS_SECRET_ACCESS_KEY=minioadmin" \
   --env="AWS_DEFAULT_REGION=us-east-1" \
-  -- aws --endpoint-url http://minio.minio.svc:9000 \
+  -- --endpoint-url http://minio.minio.svc:9000 \
   s3 cp /etc/hostname s3://test-bucket/pod-b.txt &
 
 wait
@@ -28,7 +28,7 @@ kubectl run pod-check --image=amazon/aws-cli --restart=Never \
   --env="AWS_ACCESS_KEY_ID=minioadmin" \
   --env="AWS_SECRET_ACCESS_KEY=minioadmin" \
   --env="AWS_DEFAULT_REGION=us-east-1" \
-  -- aws --endpoint-url http://minio.minio.svc:9000 \
+  -- --endpoint-url http://minio.minio.svc:9000 \
   s3 ls s3://test-bucket/
 
 kubectl wait pod/pod-check --for=jsonpath='{.status.phase}'=Succeeded --timeout=60s
